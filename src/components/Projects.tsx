476771f7d81948afc8b64a83522b806d7390c29d
Mohamed Lem <68510473+MedtheVorg@users.noreply.github.com>
2024-04-20 20:31:05 +0000

    added chatShpere and devQuiz projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,18 +2,11 @@ import Project from './project';
 import marketPlaceLogo from '@/public/all_in_one_marketplace.png';
 import vStoreLogo from '@/public/vstore_logo_png.png';
 import miniStoreLogo from '@/public/MiniStore.jpg';
+import chatSphereLogo from '@/public/chatShpere.svg';
+import devQuizLogo from '@/public/devRecruitIcon.ico';
 
 export default function Projects() {
     const projectsDetails = [
-        {
-            title: 'MarketPlace',
-            description:
-                ' Single product E-commerce web application built with NextJs and is backed and controlled by the VStore CMS Dashboard',
-            logo: marketPlaceLogo,
-            githubLink:
-                'https://github.com/MedtheVorg/V-Store/tree/main/MarketPlace_Store',
-            livePreviewUrl: 'https://marketplace-store-eight.vercel.app/',
-        },
         {
             title: 'VStore',
             description:
@@ -24,12 +17,37 @@ export default function Projects() {
             livePreviewUrl: 'https://v-store-dashboard-client.vercel.app/',
         },
         {
+            title: 'MarketPlace',
+            description:
+                ' Single product E-commerce web application built with NextJs and is backed and controlled by the VStore CMS Dashboard',
+            logo: marketPlaceLogo,
+            githubLink:
+                'https://github.com/MedtheVorg/V-Store/tree/main/MarketPlace_Store',
+            livePreviewUrl: 'https://marketplace-store-eight.vercel.app/',
+        },
+        {
             title: 'MiniStore',
             description: 'A Full stack ecommerce web application (MERN)',
             logo: miniStoreLogo,
             githubLink:
                 'https://github.com/MedtheVorg/MiniStore-Ecommerce-Website',
             livePreviewUrl: 'https://ministoree.vercel.app/',
+        },
+        {
+            title: 'ChatSphere',
+            description:
+                'Chat app that allows you to join chat rooms and chat with other members through real-time communication. built using socket.io',
+            logo: chatSphereLogo,
+            githubLink:
+                'https://github.com/MedtheVorg/ChatSphere--websocket-chat-app-',
+            livePreviewUrl: 'https://chatsphere-seven.vercel.app',
+        },
+        {
+            title: 'DevQuiz',
+            description: 'Quiz app built using React and its ecosystem',
+            logo: devQuizLogo,
+            githubLink: 'https://github.com/MedtheVorg/DevRecruit-Quiz-App',
+            livePreviewUrl: 'https://dev-recruit-quiz-app.vercel.app',
         },
     ];
     return (
